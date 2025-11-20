0bbe7fa95e3ebbb589923335cd21f61a2c19bed2
Robert Terupo <robert.terupo@gmail.com>
2025-11-21 06:19:16 +1000

    Adding modular structures

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// assets/script.js
+document.querySelectorAll('.nav a').forEach(a => {
+  a.addEventListener('click', e => {
+    const id = a.getAttribute('href');
+    const el = document.querySelector(id);
+    if (el) {
+      e.preventDefault();
+      el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  });
+});
+
